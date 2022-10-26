ae3c7c3238210cb6d89f4b46cb4c280e78f2c5fc
Rob Wilson <roobert@gmail.com>
2022-10-26 21:58:56 +0100

    Adjust lualine colors

--- lua/lualine/themes/nightshift.lua
+++ lua/lualine/themes/nightshift.lua
@@ -1,60 +1,51 @@
 -- stylua: ignore start
 
 local colors = {
-  color2 = '#161821', -- hsl('#161821')
-  color4 = '#6b7089', -- hsl('#6b7089')
-
-  color3 = '#b4be82', -- hsl('#b4be82')
-  color5 = '#2e313f', -- hsl('#2e313f')
-
-  color10 = '#0f1117', -- hsl('#0f1117')
-  color12 = '#818596', -- hsl('#818596')
-
-  color8 = '#e2a478', -- hsl('#e2a478')
-
-  color9 = '#3e445e', -- hsl('#3e445e')
-
-  color11 = '#17171b', -- hsl('#17171b')
-
-  color15 = '#84a0c6', -- hsl('#84a0c6')
-
-  -- FIXME
-  -- darken these?
-  green = "#99dddd",
-  blue = "#99ccff",
-  yellow = "#ffeedd",
-  red = "#ffaaaa",
+  -- slightly darkened accent colors
+  white     = "#dddddd",
+  grey      = "#888888",
+  black     = "#000000",
+  green     = "#88cccc",
+  blue      = "#88bbee",
+  yellow    = "#ffeedd",
+  red       = "#ee9999",
+  dark_blue = "#232a4d",
+  mid_blue  = "#232a4d",
 }
 
 return {
+  --
+  -- | A | B | C ------------- X | Y | Z
+  --
+
   inactive = {
-    c = { fg = colors.color9, bg = colors.color10 },
-    a = { fg = colors.color9, bg = colors.red, gui = 'bold' },
-    b = { fg = colors.color9, bg = colors.color10 },
+    c = { fg = colors.grey, bg = colors.dark_blue },
+    a = { fg = colors.white, bg = colors.red, gui = 'bold' },
+    b = { fg = colors.white, bg = colors.mid_blue },
   },
 
   -- grey
   normal = {
-    c = { fg = colors.color9, bg = colors.color10 },
-    a = { fg = colors.color11, bg = colors.color12, gui = 'bold' },
-    b = { fg = colors.color4, bg = colors.color5 },
+    c = { fg = colors.grey, bg = colors.dark_blue },
+    a = { fg = colors.black, bg = colors.grey, gui = 'bold' },
+    b = { fg = colors.white, bg = colors.mid_blue },
   },
 
   -- green
   insert = {
-    a = { fg = colors.color2, bg = colors.green, gui = 'bold' },
-    b = { fg = colors.color4, bg = colors.color5 },
+    a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+    b = { fg = colors.white, bg = colors.mid_blue },
   },
 
   -- blue
   visual = {
-    a = { fg = colors.color2, bg = colors.blue, gui = 'bold' },
-    b = { fg = colors.color4, bg = colors.color5 },
+    a = { fg = colors.black, bg = colors.blue, gui = 'bold' },
+    b = { fg = colors.grey, bg = colors.mid_blue },
   },
 
   -- orange
   replace = {
-    a = { fg = colors.color2, bg = colors.red, gui = 'bold' },
-    b = { fg = colors.color4, bg = colors.color5 },
+    a = { fg = colors.black, bg = colors.red, gui = 'bold' },
+    b = { fg = colors.grey, bg = colors.mid_blue },
   },
 }
