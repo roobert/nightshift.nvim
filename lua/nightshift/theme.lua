6fbd6805b5d31ffc7dc13948a8849fc8fc8171eb
Rob Wilson <roobert@gmail.com>
2022-11-26 23:26:11 +0000

    Switch to relative colors

--- lua/nightshift/theme.lua
+++ lua/nightshift/theme.lua
@@ -43,44 +43,43 @@ local theme = lush(function(injected_functions)
   local warn  = accent_orange
   local error = accent_red
 
-  local c01 = hsl("#232a4d") -- c01 is the main colour for highlighted/selected stuff
-  local c02 = hsl("#364178") -- menu sbar/thumb
-  local c03 = hsl("#3e4b8a") -- include/function, etc.
-  local c04 = hsl("#46549b") -- comment/keyword
+  -- local c01 = hsl("#232a4d") -- c01 is the main colour for highlighted/selected stuff
+  -- local c02 = hsl("#364178") -- menu sbar/thumb
+  -- local c03 = hsl("#3e4b8a") -- include/function, etc.
+  -- local c04 = hsl("#46549b") -- comment/keyword
 
-  local c05 = hsl("#5664b3") -- 1 - just line number
-  local c06 = hsl("#6b75bb") -- 2 - field/special
-  local c07 = hsl("#8185c2") -- 2 - operator/shebang
-  local c08 = hsl("#9696ca") -- 3 - normal text
+  -- local c05 = hsl("#5664b3") -- 1 - just line number
+  -- local c06 = hsl("#6b75bb") -- 2 - field/special
+  -- local c07 = hsl("#8185c2") -- 2 - operator/shebang
+  -- local c08 = hsl("#9696ca") -- 3 - normal text
 
-  local c09 = hsl("#b9bfdf") -- unused
-  local c10 = hsl("#bfbfdf") -- variable/field/identifier
-  local c11 = hsl("#d8dbee") -- URIs
+  -- local c09 = hsl("#b9bfdf") -- unused
+  -- local c10 = hsl("#bfbfdf") -- variable/field/identifier
+  -- local c11 = hsl("#d8dbee") -- URIs
 
-  -- NOTE:
+  -- FIXME:
   -- adjust all this stuff with .rotate() .(de)saturate() and (ligh|dark)en()
 
-  -- the main spectrum/gradient goes from dark to light and consists
-
-  -- local color_main = hsl("#46549b")
-
   -- local color_main = hsl("#0000ff").rotate(-15).desaturate(50).darken(30)
-  -- local color_secondary = color_main.lighten(60)
-  -- local color_thirdary = color_secondary.lighten(80)
+  local color_main = hsl("#232a4d") -- c01 is the main colour for highlighted/selected stuff
 
-  -- local color01 = color_thirdary
-  -- local color02 = color01.darken(10)
-  -- local color03 = color02.darken(10)
+  local c01 = color_main
+  local c02 = c01.lighten(10)
+  local c03 = c02.lighten(10)
+  local c04 = c03.lighten(10)
 
-  -- local color04 = color_secondary
-  -- local color05 = color04.darken(10)
-  -- local color06 = color05.darken(10)
-  -- local color07 = color06.darken(10)
+  local color_secondary = color_main.lighten(30)
 
-  -- local color08 = color_main
-  -- local color09 = color08.darken(10)
-  -- local color10 = color09.darken(10)
-  -- local color11 = color10.darken(40)
+  local c05 = color_secondary
+  local c06 = c05.lighten(10)
+  local c07 = c06.lighten(10)
+  local c08 = c07.lighten(10)
+
+  local color_thirdary = color_secondary.lighten(30)
+
+  local c09 = color_thirdary
+  local c10 = c09.lighten(45)
+  local c11 = c10.lighten(10)
 
   -- local accent_primary = color_secondary.saturate(90).lighten(20)
 
@@ -128,6 +127,10 @@ local theme = lush(function(injected_functions)
     PmenuSbar { bg = c02 }, -- Popup menu: scrollbar.
     PmenuThumb { bg = c02 }, -- Popup menu: Thumb of the scrollbar.
 
+    -- used by shebang lines
+    PreProc { fg = c07 }, -- (preferred) generic Preprocessor
+    TSURI { fg = c11 }, -- Any URI like a link or email.
+
     -- cursor line / visual selection backgrounds
     --Cursor { fg = String.fg, bg = red }, -- character under the cursor - unused?
     lCursor {}, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
@@ -155,11 +158,11 @@ local theme = lush(function(injected_functions)
     Float { fg = accent_blue }, -- a floating point constant: 2.3e10
 
     -- search/match
-    MatchParen { fg = accent_green }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
     CurSearch { fg = search_highlight.darken(50) },
     IncSearch { fg = search_highlight.darken(50) }, -- 'incsearch' highlighting; also used for the text replaced      with  ":s///c"
     Search { fg = search_highlight.darken(15) },
     Substitute { fg = search_highlight.darken(15) }, -- |:substitute| replacement text  highlighting
+    MatchParen { fg = accent_green }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
 
     -- TODO, FIXME, etc.
     WarningMsg { fg = black, bg = warn }, -- warning messages
@@ -181,12 +184,7 @@ local theme = lush(function(injected_functions)
     -- diff stuff
     DiffAdd { fg = accent_green.darken(15) }, -- diff mode: Added line |diff.txt|
     DiffChange { fg = accent_blue.darken(15) }, -- diff mode: Changed line |diff.txt|
-    DiffDelete { fg = accent_red.darken(15) }, -- diff mode: Deleted line |diff.txt|
-
-    TSURI { fg = c11 }, -- Any URI like a link or email.
-
-    -- used by shebang lines
-    PreProc { fg = c07 }, -- (preferred) generic Preprocessor
+    DiffDelete { fg = accent_red.darken(15) } -- diff mode: Deleted line |diff.txt|
   }
 end)
 
